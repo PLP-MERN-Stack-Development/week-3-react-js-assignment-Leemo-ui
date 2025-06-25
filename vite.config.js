cdea0a4337991d2663ae9c77b8d61fc5809dcb33
Leemo-ui <merkitjohn@gmail.com>
2025-06-25 16:04:23 +0300

    feat: add path-type module with async and sync methods for file, directory, and symlink checks
    feat: implement pkg-dir module to find the root directory of a Node.js project or npm package
    
    feat: introduce slash module to convert Windows backslash paths to forward slash paths
    
    feat: add strip-outer module to remove a substring from the start and end of a string
    
    feat: implement trim-repeated module to condense consecutively repeated substrings
    
    feat: add universalify module to support both promises and callbacks in functions

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/week-3-react-js-assignment-Leemo-ui/', // IMPORTANT: Your repo name
   plugins: [react()],
 });
